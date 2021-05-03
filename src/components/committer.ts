0736f12e0d72ad4ac46792bf5bf387dad9c813b2
Claudio Henrique <HacskBoy@hotmail.com>
2021-05-02 22:20:04 -0300

    refactor: change type vars

--- src/components/committer.ts
+++ src/components/committer.ts
@@ -1,6 +1,6 @@
-var commit = function(msg){
+const commit = function(msg){
     const { exec } = require("child_process");
-    var commit = 'git commit -m "'+msg+'"';
+    const commit = 'git commit -m "'+msg+'"';
     exec(commit, (error, stdout, stderr) => {
         console.log(stderr ? 'Commit não realizado.' : 'Commit realizado, realize o push.');
     });
