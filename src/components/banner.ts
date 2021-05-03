896dd7e8dcbd6f357f0d0f1c869e36b2ed351fe0
Claudio Henrique <HacskBoy@hotmail.com>
2021-05-03 20:23:12 -0300

    refactor: destroys import in banner

--- src/components/banner.ts
+++ src/components/banner.ts
@@ -1,10 +1,10 @@
-import * as chalk from 'chalk';
-import * as figlet from 'figlet';
+import { cyan } from 'chalk';
+import { textSync } from 'figlet';
 
 const banner = () => {
   console.log(
-    chalk.cyan(
-      figlet.textSync('GIT LINT', {
+    cyan(
+      textSync('GIT LINT', {
         horizontalLayout: 'full',
       })
     )
