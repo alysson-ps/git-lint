7b44ccee06af26468aebc600e22ef6f8947f25a2
Alysson porto <craftgames.alysson@gmail.com>
2021-09-08 22:48:10 -0300

    refactor[]: remove file not necessary

--- teste.js
+++
@@ -1,9 +0,0 @@
-const { getDictionarySync } = require('simple-spellchecker');
-
-const SpellCheckerNode = (words = ['uma', 'safada', 'de', 'saia', 'curta']) => {
-  // let wordsWithError = [];
-  const dict = getDictionarySync('en-US');
-  return words.map((word) => dict.spellCheck(word));
-  // dict.spellCheck()
-};
-console.log(SpellCheckerNode());
